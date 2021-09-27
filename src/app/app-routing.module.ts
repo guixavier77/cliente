b814348bf16e4ca29a871cae36cefde71d6e9e05
guixaiver77 <xguilherme1@gmail.com>
2021-09-27 20:59:10 -0300

    teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: 'entregas',
-    loadChildren: () => import('./acceptentregas/entregas.module').then( m => m.EntregasPageModule)
+    loadChildren: () => import('./cad/entregas.module').then( m => m.EntregasPageModule)
   }
 
 ];
